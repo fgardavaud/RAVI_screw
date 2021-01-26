1e6d6388adec6fa88d690a0a3c83f9b74e59b568
fgardavaud <francois.gardavaud@aphp.fr>
2021-01-26 17:31:15 +0100

    initial commit. it remains to filter patients names.

---
+++ Data_pretreatment.R
@@ -0,0 +1,221 @@
+######################################################################
+##                                                                  ##                                  
+##                      Mammo comparison study                      ##
+##                    to compare WIQ with Dose data                 ##
+##                                                                  ##                                 
+######################################################################
+
+# created by François Gardavaud
+# date : 01/26/2021
+
+###################### set-up environment section ################################
+
+# Set the project path to the root level -
+root.dir = rprojroot::find_rstudio_root_file()
+
+# # load readxl package to read easyly Excel file with an install condition
+# if(!require(readxl)){
+#   install.packages("readxl")
+#   library(readxl)
+# }
+
+# load lubridate package to determine patient age from birthdate with an install condition
+if(!require(lubridate)){
+  install.packages("lubridate")
+  library(lubridate)
+}
+
+# load doMC package for parallel computing
+if(!require(doMC)){
+  install.packages("doMC")
+  library(doMC)
+}
+# load foreach package for parallel computing on for loop
+if(!require(foreach)){
+  install.packages("foreach")
+  library(foreach)
+}
+# load doparallel package to implement parallel computing
+# if(!require(doParallel)){
+#   install.packages("doParallel")
+#   library(doParallel)
+# }
+# load tictoc package to measure running time of R code
+if(!require(tictoc)){
+  install.packages("tictoc")
+  library(tictoc)
+}
+# load excel package to write results in Excel file
+if(!require(openxlsx)){
+  install.packages("openxlsx")
+  library(openxlsx)
+}
+# load dplyr for data handling
+if(!require(dplyr)){
+  install.packages("dplyr")
+  library(dplyr)
+}
+# load prettyR package for better statistical analysis
+if(!require(prettyR)){
+  install.packages("prettyR")
+  library(prettyR)
+}
+# # load sulmarytoolspackage for better table output
+# if(!require(summarytools)){
+#   install.packages("summarytools")
+#   library(summarytools)
+# }
+
+###############################################################################################################
+###############################################################################################################
+####################### First part : Match patient IPP between PACS and anapath file ##########################
+###############################################################################################################
+###############################################################################################################
+
+
+
+############################### data import section ##################################
+# read the database with data frame existing test
+# my_all_data <- read_excel("data/CV-IR_Tenon_Radiologie_detailed_data_export_tronque.xlsx")
+
+## /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
+# unfortunately Excel importation yield to parse errors in date data for instance.
+# SO TO AVOID THIS PROBLEM THE USER HAVE TO IMPORT DATA IN CSV FORMAT 
+# BY CONVERTING ORIGINAL DATA FROM .XLSX TO .CSV IN EXCEL SOFTWARE
+# /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
+
+tic("to import mammo patient data in Rstudio")
+if(exists("my_all_data")){
+  print("raw data importation have already done")
+}else{
+  DoseWatch_export <- read.csv2("data/CV-IR_Tenon_Radiologie_detailed_data_export.csv", sep = ";")
+  
+}
+toc()
+
+################################## data tailoring section ##################################
+# data filter to keep only interested columns for this study
+DoseWatch_Selected_data <- DoseWatch_export %>% select(Study.date..YYYY.MM.DD., Accession.number,
+                                                       Patient.birthdate..YYYY.MM.DD.,
+                                                       Patient.weight..kg., Patient.size..cm.,
+                                                       BMI, Standard.study.description, Performing.physician.last.name,
+                                                       Peak.Skin.Dose..mGy.,
+                                                       Image.and.Fluoroscopy.Dose.Area.Product..mGy.cm2.,
+                                                       Total.Acquisition.DAP..mGy.cm..,Total.Fluoro.DAP..mGy.cm..,
+                                                       Total.Air.Kerma..mGy.,
+                                                       Total.Acquisition.Air.Kerma..mGy., Total.Fluoro.Air.Kerma..mGy.,
+                                                       Total.Time.of.Fluoroscopy..s., Number.of.Acquisition.Series,
+                                                       Irradiation.Event.Type,Proprietary.Type, Dose.Preference,
+                                                       Positioner.Primary.Angle..deg., Positioner.Secondary.Angle..deg.,
+                                                       Field.of.View..cm.) # to select column of interest and keeping the column's name
+
+######################## age patient computation #################################
+
+#  instance null vector with appropriate dimension to bind with Study_data
+Patient.Age <- rep(0, nrow(DoseWatch_Selected_data))
+
+# Loop with parallelization to calculate patient age in years 
+# and add this information to Study_data dataframe
+# also have a condition to test global environement objet for debugging
+tic("for loop with parallelization")
+if(exists("Study_data_age")){
+  print("patient age computation have already done")
+}else{
+  cores <- detectCores()
+  registerDoMC(cores - 1)
+  Patient.Age <- foreach(i = 1:nrow(DoseWatch_Selected_data)) %dopar% {
+    naiss = ymd_hms(DoseWatch_Selected_data[i,3])
+    evt = as.POSIXct(DoseWatch_Selected_data[i,1])
+    age = as.period(interval(naiss, evt))@year
+    Patient.Age <- age
+  }
+}
+toc()
+
+Patient.Age <- as.character(Patient.Age)
+Study_data_age <-cbind(DoseWatch_Selected_data,Patient.Age)
+
+Study_data_selected_age <- Study_data_age %>% select(Study.date..YYYY.MM.DD., Accession.number,
+                                                     Patient.Age,
+                                                     Patient.weight..kg., Patient.size..cm.,
+                                                     BMI, Standard.study.description, Performing.physician.last.name,
+                                                     Peak.Skin.Dose..mGy.,
+                                                     Image.and.Fluoroscopy.Dose.Area.Product..mGy.cm2.,
+                                                     Total.Acquisition.DAP..mGy.cm..,Total.Fluoro.DAP..mGy.cm..,
+                                                     Total.Air.Kerma..mGy.,
+                                                     Total.Acquisition.Air.Kerma..mGy., Total.Fluoro.Air.Kerma..mGy.,
+                                                     Total.Time.of.Fluoroscopy..s., Number.of.Acquisition.Series,
+                                                     Irradiation.Event.Type,Proprietary.Type, Dose.Preference,
+                                                     Positioner.Primary.Angle..deg., Positioner.Secondary.Angle..deg.,
+                                                     Field.of.View..cm.)
+
+
+
+############### column format conversion #################
+# convert patient years in numeric
+Study_data_selected_age$Patient.Age <- as.numeric(Study_data_selected_age$Patient.Age)
+Study_data_selected_age$Total.Acquisition.DAP..mGy.cm.. <- as.numeric(Study_data_selected_age$Total.Acquisition.DAP..mGy.cm..)
+Study_data_selected_age$Total.Fluoro.DAP..mGy.cm.. <- as.numeric(Study_data_selected_age$Total.Fluoro.DAP..mGy.cm..)
+
+############### retrieve study patient lines #################
+
+# add a filter to exam description
+Study_data_selected_exam <- Study_data_selected_age %>% filter(Standard.study.description == "CIMENTOPLASTIE" |
+                                                                 Standard.study.description == "OSTEO BASSIN" | Standard.study.description == "OSTEOSYNTHESE BASSIN" |
+                                                                 Standard.study.description == "VERTEBROPLASTIE") 
+# # add a filter to patient name
+# Study_data_selected_exam <- Study_data_selected_age %>% filter(Standard.study.description == "CIMENTOPLASTIE" |
+#                                                                  Standard.study.description == "OSTEO BASSIN" | Standard.study.description == "OSTEOSYNTHESE BASSIN" |
+#                                                                  Standard.study.description == "VERTEBROPLASTIE") 
+# 
+
+
+################## Global environment cleaning ###########################
+
+# Remove dataframe which don't still have any interest
+if(exists("Patient.Age")) {
+  print("Global environment will be clean")
+  rm (Patient.Age, DoseWatch_Selected_data, Study_data_selected_age)
+}else{
+  print("Global environment already clean")
+}
+
+
+###############################################################################################################
+###############################################################################################################
+############# Second part : Bin data to express dose in function to angle #####################################
+###############################################################################################################
+###############################################################################################################
+
+##########################################################################
+##########################################################################
+##########################################################################
+######################## Some statistics #################################
+##########################################################################
+##########################################################################
+##########################################################################
+
+Global_stat <- summary(Patient_merge_data_all_source_selected)
+write.xlsx(Global_stat, 'output/Global_stat.xlsx', sheetName = "Global_stat",
+           col.names = TRUE, row.names = TRUE, append = FALSE)
+
+# Global_stat_test <- describe(Patient_merge_data_all_source_selected, num.desc=c("mean","median","sd","min","max","valid.n"))
+# write.xlsx(Global_stat_test, 'output/Global_stat_test.xlsx', sheetName = "Global_stat_test",
+#           col.names = TRUE, row.names = TRUE, append = FALSE)
+
+
+Tomo.Number <- length(Patient_merge_data_all_source$Infos.suppl)
+ExamAET.stat <- Patient_merge_data_all_source %>%
+  group_by(AE.source) %>%
+  summarise(
+    count = n(),
+  )
+write.xlsx(ExamAET.stat, 'output/ExamAET.stat.xlsx', sheetName = "ExamAET.stat",
+           col.names = TRUE, row.names = TRUE, append = FALSE)
+
+# CBCT_position <- which(Study_data_prostate$Irradiation.Event.Type == "CBCT+")
+# Exam_ID_list_CBCT <- table(Study_data_prostate_CBCT$Study_data_prostate_CBCT.Accession.number, droplevels(Study_data_prostate_CBCT$Study_data_prostate_CBCT.Irradiation.Event.Type))
+# Study_data_prostate_CBCT <- subset(Study_data_prostate, Irradiation.Event.Type == "CBCT+")
+
+
+
